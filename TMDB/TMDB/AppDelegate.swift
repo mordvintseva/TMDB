681a6d6b293167ac700a5cb1fb87da74964474bc
Mordvintseva Alena <mordvinceva1999@gmail.com>
2020-06-23 07:40:55 -0700

    Init project

---
+++ TMDB/TMDB/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  TMDB
+//
+//  Created by Alena on 23/06/2020.
+//  Copyright © 2020 Alena. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    // swiftlint:disable:next line_length
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ TMDB/TMDB/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TMDB
+//
+//  Created by Alena on 23/06/2020.
+//  Copyright © 2020 Alena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
