1bcfe0eb62f4231e20589c8619f9db91149bc3eb
Mordvintseva Alena <mordvinceva1999@gmail.com>
2020-06-24 08:52:02 -0700

    Create login screen

--- TMDB/TMDB/ViewController.swift
+++ TMDB/TMDB/ViewController.swift
@@ -1,16 +1,52 @@
-//
-//  ViewController.swift
-//  TMDB
-//
-//  Created by Alena on 23/06/2020.
-//  Copyright © 2020 Alena. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    // MARK: - IBOutlets
+
+    @IBOutlet var emailLabel: UILabel!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordLabel: UILabel!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var entryButton: UIButton!
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupTextFields()
+        setOutletsText()
+    }
+
+}
+
+// MARK: - Private Methods
+
+private extension ViewController {
+    func setupTextFields() {
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+
+        emailTextField.tag = 0
+        passwordTextField.tag = 1
+    }
+
+    func setOutletsText() {
+        emailLabel.text = "Email"
+        passwordLabel.text = "Password"
+        entryButton.setTitle("Entry", for: .normal)
+    }
+
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let nextTextField = textField.superview?.viewWithTag(textField.tag + 1) as? UITextField {
+            nextTextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        return false
     }
 }
